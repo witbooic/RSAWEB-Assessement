1042d19f31ab8b8df6c8d7449f8d87af334c6d78
witbooic <42607898+witbooic@users.noreply.github.com>
2023-08-22 18:38:33 +0200

    Add files via upload

---
+++ chad-rsaweb-terraform.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-053b0d53c279acc90"  
+  instance_type = "t2.micro"              
+
+  tags = {
+    Name = "Chad-RSAWEB"
+  }
+}

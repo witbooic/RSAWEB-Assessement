9b6d12eab58037b0683e1d2ec2b2bd87a2dd4cda
witbooic <42607898+witbooic@users.noreply.github.com>
2023-08-23 19:04:59 +0200

    Update ec2build-rsaweb-terraform.tf

--- ec2build-rsaweb-terraform.tf
+++ ec2build-rsaweb-terraform.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 }
 
-resource "aws_instance" "example_instance" {
+resource "aws_instance" "chadrsaweb_instance" {
   ami           = "ami-053b0d53c279acc90"  
   instance_type = "t2.micro"              
 
